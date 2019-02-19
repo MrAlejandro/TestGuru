d724b8e94a8e9682ed29e489757924101ccc65fc
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-02-19 21:22:05 +0400

    Application model added.

---
+++ db/migrate/20190219164255_create_answers.rb
@@ -0,0 +1,12 @@
+class CreateAnswers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :answers do |t|
+      t.string :body, null: false
+      t.boolean :correct, null: false, default: false
+      t.integer :question_id
+      t.references :question, foreign_key:true
+
+      t.timestamps
+    end
+  end
+end
