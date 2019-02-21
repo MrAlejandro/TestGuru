95ab3b9af5143dee997dca96d3f580fda163054e
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-02-21 21:49:14 +0400

    Class name replaced with self.

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   has_many :users, through: :test_results
 
   def self.find_by_category(category_name)
-    Test
+    self
       .select("test.title")
       .joins("JOIN categories ON categories.id = tests.category_id")
       .where("categories.title LIKE ?", "%#{category_name}%")
