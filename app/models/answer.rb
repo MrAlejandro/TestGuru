24bb07dab495bfa38c9d09137b180b0f5a76b40a
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-02-24 13:05:44 +0400

    Answer on question dependency optionality removed.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,15 +2,13 @@ class Answer < ApplicationRecord
   validates :body, presence: true
   validate :validate_question_answers_number
 
-  belongs_to :question, optional: true
+  belongs_to :question
 
   scope :correct, -> { where(correct: true) }
 
   private
 
   def validate_question_answers_number
-    if question && question.num_of_answers >= 4
-      errors.add(:answers, "Parent question already have 4 answers.")
-    end
+    errors.add(:answers, "question already has 4 answers.") if question.num_of_answers >= 4
   end
 end
