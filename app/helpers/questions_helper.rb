11a30960772a4ec30a89fa6b50adbee6819b626a
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-02-27 22:13:35 +0400

    Footer and helpers added.

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_title(test, question)
+    if question.persisted?
+      "Edit '#{test.title}' question"
+    else
+      "Create '#{test.title}' question"
+    end
+  end
 end
