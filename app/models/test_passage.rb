7c31e432108cf01fb592f96e70609cb8c5ec20b8
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-03-01 22:56:36 +0400

    Tests results page and navigation improved.

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,16 +1,15 @@
 class TestPassage < ApplicationRecord
+  PASSAGE_PERCENT = 85.0
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
+  before_update :before_update_set_next_question
   before_validation :before_validation_set_first_question, on: :create
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
-
-    self.current_question = next_question
+    self.correct_questions += 1 if correct_answer?(answer_ids)
     save!
   end
 
@@ -18,25 +17,50 @@ class TestPassage < ApplicationRecord
     current_question.nil?
   end
 
+  def passed?
+    score >= PASSAGE_PERCENT
+  end
+
+  def score
+    (correct_questions / questions_number.to_f) * 100
+  end
+
+  def questions_number
+    test.questions.count
+  end
+
+  def current_question_number
+    test
+      .questions
+      .where("id <= ?", current_question.id)
+      .count
+  end
+
+  def to_s
+    "#{score} %"
+  end
+
   private
 
   def correct_answer?(answer_ids)
-    correct_answers_number = correct_answers.count
-
-    correct_answers_number == answer_ids.count &&
-      correct_answers_number == correct_answers.where(id: answer_ids).count
+    return false if answer_ids.nil? || answer_ids.empty?
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
     current_question.answers.correct
   end
 
+  def before_update_set_next_question
+    self.current_question = next_question
+  end
+
   def next_question
     test
-      .questions
-      .where("id > ?", self.current_question)
-      .order(id: :DESC)
-      .first
+        .questions
+        .where("id > ?", self.current_question)
+        .order(id: :DESC)
+        .first
   end
 
   def before_validation_set_first_question
