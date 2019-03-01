828928cb1e25803a6ed009caf5d4f87b395310df
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-03-01 21:39:20 +0400

    Worked trough scrincasts.

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,12 +3,43 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
+  before_validation :before_validation_set_first_question, on: :create
 
-    # before_validation :before_validation_set_first_question, on :create
-  #
-  # private
-  #
-  # def before_validation_set_first_question
-  #   self.current_question = test.questions.first if test.present?
-  # end
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+
+    self.current_question = next_question
+    save!
+  end
+
+  def completed?
+    current_question.nil?
+  end
+
+  private
+
+  def correct_answer?(answer_ids)
+    correct_answers_number = correct_answers.count
+
+    correct_answers_number == answer_ids.count &&
+      correct_answers_number == correct_answers.where(id: answer_ids).count
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    test
+      .questions
+      .where("id > ?", self.current_question)
+      .order(id: :DESC)
+      .first
+  end
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
 end
