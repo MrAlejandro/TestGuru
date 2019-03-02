a6090700f1ee241052dc04712526620838e95b04
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-03-02 19:19:37 +0400

    User authorization added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
+require "digest/sha1"
+
 class User < ApplicationRecord
-  validates :name, presence: true
+  include Auth
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :created_tests, class_name: "Test", foreign_key: :author_id
+
+  has_secure_password
+
+  validates :name, presence: true
 
   def find_test_by_level(level)
     tests.where(level: level)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_180049) do
+ActiveRecord::Schema.define(version: 2019_03_02_142406) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2019_02_28_180049) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
