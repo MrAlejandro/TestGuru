494113afaee988b53f334daaecf5a5b958da7b7c
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-03-02 20:21:04 +0400

    Flash messages logic added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
 
   private
 
   def authenticate_user!
-    redirect_to login_path unless current_user
+    redirect_to login_path, alert: "Are you a Guru? Verify your Email and Password please" unless current_user
+    cookies[:email] = current_user&.email
   end
 
   def current_user
