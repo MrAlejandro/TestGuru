fc9f7a9bc7b2795517af71b923953f953b61d389
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-03-03 13:25:10 +0400

    Admin resources moved to a namespace.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,44 +1,10 @@
 class TestsController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :set_test, only: %i[show edit update destroy start]
+  before_action :set_test, only: :start
 
   def index
     @tests = Test.all
-  end
-
-  def show
-    render locals: {category: @test.category.title}
-  end
-
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = Test.new(test_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
   end
 
   def start
@@ -50,9 +16,5 @@ class TestsController < ApplicationController
 
   def set_test
     @test = Test.find(params[:id])
-  end
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id)
   end
 end
