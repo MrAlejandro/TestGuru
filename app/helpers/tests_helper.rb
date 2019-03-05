31a143532f65c863d9f341ec802a9d5b40a5679b
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-03-05 21:20:55 +0400

    Admin and user tests and test passages localizations added.

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -2,6 +2,7 @@ module TestsHelper
   TEST_LEVELS = { 0 => :easy, 1 => :elementary, 2 => :advanced, 3 => :hard }.freeze
 
   def test_level(test)
-    TEST_LEVELS[test.level] || :hero
+    level_code = TEST_LEVELS[test.level] || :hero
+    t("helpers.tests.level.#{level_code.to_s}")
   end
 end
